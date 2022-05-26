8c1b945303c506780a82f57a795457ac79684704
AhmedmMhmoud2001 <ahmedmahmoud2152001@gmail.com>
2022-05-26 10:39:39 +0200

    first commit

---
+++ website/app.js
@@ -0,0 +1,9 @@
+/* Global Variables */
+document.getElementById('zip')
+    // expected output: "resolved"
+  
+  
+  asyncCall();
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
